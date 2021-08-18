f53a5a84d4e7965d65956cfba60b2762cbd05dbc
gforestier <forestier_guillaume@yahoo.com>
2021-08-18 14:04:33 +0200

    update region vpc security group

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "security_group_id" {
-  default = "sgr-030b57788f2cba527"
+  default = "sg-0eb788d488a25464e"
 }
 
 variable "subnet_id" {
-  default = "subnet-a00becd9"
+  default = "subnet-bf5474e5"
 }
 
 variable "region" {
